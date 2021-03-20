0f711c315f9616df3d395ce1c2a2508428a7e867
Ryan Greenhalgh <ryan.greenhalgh@hotmail.co.uk>
2021-03-20 12:15:23 +0000

    shell

--- scripts/get_example_data.sh
+++ scripts/get_example_data.sh
@@ -5,5 +5,4 @@
 if [ ! -d "data" ]; then
     wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1cboy6pBr8PQW1PqEyRl3lDSjGUPMFoTN' -O data.zip && \
     unzip data.zip                                  && \
-    rm data.zip
 fi
