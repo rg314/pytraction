586c587e8791c0ae9a1e7088dbaa3d286ab3de87
Ryan Greenhalgh <ryan.greenhalgh@hotmail.co.uk>
2021-03-20 12:32:53 +0000

    fix for DSSTORE files on mac

--- scripts/get_example_data.sh
+++ scripts/get_example_data.sh
@@ -3,6 +3,7 @@
 # data_20210320.zip
 
 if [ ! -d "data" ]; then
-    wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1cboy6pBr8PQW1PqEyRl3lDSjGUPMFoTN' -O data.zip && \
+    wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1DsPuqAzI7CEH-0QN-DWHdnF6-to5HdFe' -O data.zip && \
     unzip data.zip                                  && \
+    rm data.zip
 fi
