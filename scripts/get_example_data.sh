2649815c55d4487200fdfa92289bd09285935208
Ryan Greenhalgh <ryan.greenhalgh@hotmail.co.uk>
2021-03-20 11:42:59 +0000

    sh script playing up

--- scripts/get_example_data.sh
+++ scripts/get_example_data.sh
@@ -3,7 +3,7 @@
 # data_20210320.zip
 
 if [ ! -d "data" ]; then
-    wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1cboy6pBr8PQW1PqEyRl3lDSjGUPMFoTN' -O data.zip  && \
+    wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1cboy6pBr8PQW1PqEyRl3lDSjGUPMFoTN' -O data.zip && \
     unzip data.zip                                  && \
     rm data.zip
 fi
