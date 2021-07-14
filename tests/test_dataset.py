3ccd5c8afe3c6f047e83b921057511f63ee7d96a
Ryan Greenhalgh <ryan.greenhalgh@hotmail.co.uk>
2021-07-14 19:36:13 +0100

    added tests for dataset

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -26,6 +26,9 @@ def test_Dataset_load():
 
     assert isinstance(dataset[0], pd.core.frame.DataFrame)
 
+    for col in dataset.columns:
+        assert isinstance(dataset[col], pd.core.frame.DataFrame)
+
 
 def test_Dataset_metadata():
 
@@ -34,6 +37,34 @@ def test_Dataset_metadata():
     dataset = Dataset(dataset_path)
 
     assert hasattr(dataset, 'metadata')
+
+    metadata = dataset.metadata()
+
+    assert isinstance(metadata, dict)
+
+    keys = [
+        'E', 
+        'coarse_factor', 
+        'dt', 
+        'meshsize', 
+        'min_window_size', 
+        'nb_iter_max', 
+        'overlap_ratio', 
+        'pix_per_mu', 
+        's', 
+        'sig2noise_method', 
+        'tolerance', 
+        'trust_1st_iter', 
+        'validation_iter', 
+        'validation_method',
+        ]
+
+
+    assert len(metadata) == len(keys)
+
+    for key in keys:
+        assert key in metadata.keys()
+
 
 
 def test_Dataset_save():
@@ -45,3 +76,16 @@ def test_Dataset_save():
     assert dataset.save('test.h5') == True
 
 
+def test_Dataset_str():
+
+
+    dataset_path = os.path.join('tests', 'data', 'example_dataset.h5')
+
+    dataset = Dataset(dataset_path)
+
+    assert isinstance(dataset.__str__(), str)
+    assert isinstance(dataset.__repr__(), str)
+
+test_Dataset_str()
+test_Dataset_metadata()
+test_Dataset_load()
